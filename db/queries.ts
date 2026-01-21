7301097771cf3e8ed0648e2916d4f3e2da424384
Oranut Suphantamat <Oranut_s@kkumail.com>
2026-01-21 21:34:10 +0700

    create laguage Courses and user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,26 @@
 import React from 'react'
 import { cache } from "react";
 import db from "@/db/drizzle";
+import { auth } from '@clerk/nextjs/server';
+import { userProgress } from './schema';
+import { eq } from 'drizzle-orm';
+
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+    if ( !userId ) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+        activeCourse: true,
+    }
+});
+
+return data;
+})
+
 
 const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
